2f133c96cd5fb827b81e61da1e0ce4b99abdb908
airtoxin <airtoxin@icloud.com>
2016-09-21 02:01:41 +0900

    Move npm-install task to plugin

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ project generator
 
 Once `clow` installed, you can use `clow` command.
 
-Let's try `$ clow https://github.com/airtoxin/clow-template-babel/archive/master.zip my-first-project`.
+Additional plugin install needed: `$ npm install -g clow-plugin-task-npm-install`.
+
+Then, let's try `$ clow https://github.com/airtoxin/clow-template-babel/archive/master.zip my-first-project`.
 This command ask some settings, so answer it and wait a few minutes. When command finished, you got babel project template on `my-first-project` directory. Run `npm run watch`, `npm test` ... in created project directory. It works! wow!
 
 ## Documents
--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-import 'babel-polyfill';
 import path from 'path';
+import npm from 'npm';
+import glob from 'glob';
 import _BaseTask from './tasks/base-task';
 import ShellTask from './tasks/shell';
-import NpmInstallTask from './tasks/npm-install';
 import TemplateTask from './tasks/template';
 import ClowTemplateTask from './tasks/clow-template';
 
+function getClowTaskPlugins() {
+  return new Promise((resolve) => {
+    npm.load(() => {
+      const searchGlob = path.resolve(npm.globalDir, 'clow-plugin-task-*');
+      const taskPlugins = glob.sync(searchGlob)
+        .map(fp => require(fp).default); // eslint-disable-line global-require
+      resolve(taskPlugins);
+    });
+  });
+}
+
 export default async function clow(generatorDir, destDir) {
-  const shellTask = new ShellTask(generatorDir, destDir);
-  const npmInstallTask = new NpmInstallTask(generatorDir, destDir);
-  const templateTask = new TemplateTask(generatorDir, destDir);
-  const clowTemplateTask = new ClowTemplateTask(generatorDir, destDir);
+  const defaultTasks = [ShellTask, TemplateTask, ClowTemplateTask];
+  const installedTasks = await getClowTaskPlugins();
+  const taskrunners = []
+    .concat(defaultTasks, installedTasks)
+    .map(Task => new Task(generatorDir, destDir));
 
   const clowFilePath = path.resolve(generatorDir, 'clow');
   // eslint-disable-next-line global-require
   const tasks = require(clowFilePath);
 
   for (const task of tasks) {
-    await shellTask.checkAndRun(task);
-    await npmInstallTask.checkAndRun(task);
-    await templateTask.checkAndRun(task);
-    await clowTemplateTask.checkAndRun(task);
-
+    for (const runner of taskrunners) {
+      await runner.checkAndRun(task);
+    }
     // eslint-disable-next-line no-console
     console.log(); // spacing each taks logs
   }
