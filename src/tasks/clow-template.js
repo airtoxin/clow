68d2ad5d1026afc93d191a2c28b5c51c8b0515c5
airtoxin <airtoxin@icloud.com>
2016-09-21 03:46:23 +0900

    Add log

--- src/tasks/clow-template.js
+++ src/tasks/clow-template.js
@@ -14,8 +14,11 @@ export default class ClowTemplateTask extends BaseTask {
   async run(task) {
     for (const url of task.templates) {
       const templateDir = await downloadTmp(url);
+      this.log(`temporary file saved: ${templateDir}`);
 
+      this.log(`Start clow: ${url}`);
       await clow(templateDir, this.destDir);
+      this.log(`Finish clow: ${url}`);
     }
   }
 }
