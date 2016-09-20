dc7a29bb8c443c075d2f63e69e967330b05a1f8a
airtoxin <airtoxin@icloud.com>
2016-09-21 00:05:16 +0900

    Support generator's url

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
 
+import 'babel-polyfill';
 import fse from 'fs-extra';
 import path from 'path';
+import isUrl from 'is-url';
 import minimist from 'minimist';
+import { downloadTmp } from './utils';
 import clow from './index';
 
 const argv = minimist(process.argv.slice(2));
 
-const generatorDir = path.resolve(process.cwd(), argv._[0]);
-const destDir = path.resolve(process.cwd(), argv._[1]);
-
+const [rawSrc, rawDest] = argv._;
+const srcDir = path.resolve(process.cwd(), rawSrc);
+const destDir = path.resolve(process.cwd(), rawDest);
 fse.ensureDirSync(destDir);
 
+const main = isUrl(rawSrc) ?
+  downloadTmp(rawSrc).then(dirpath => clow(dirpath, destDir)) :
+  clow(srcDir, destDir);
+
 // eslint-disable-next-line no-console
-clow(generatorDir, destDir).catch(e => console.error(e));
+main.catch(e => console.error(e));
